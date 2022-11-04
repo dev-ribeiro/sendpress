b1a8fe3ea8bfdb52b5210b276d8717e3a5632680
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-03 21:37:06 -0300

    config: adjust style-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../styles/global'
+import { defaultTheme } from '../styles/themes/defaultTheme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ styles/themes/defaultTheme.ts
@@ -0,0 +1,8 @@
+export const defaultTheme = {
+  'white':'#fff',
+  'black':'#000',
+  'gray-300':'#D9D9D9',
+  'yellow-400':'#FCBC58',
+  'orange-400':'#F0945B',
+  'purple-600':'#4C489D'
+} as const
