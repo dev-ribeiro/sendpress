4e5bdd5aaba91a2472e568fa926487231b8a5513
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-05 20:10:28 -0300

    styles: adjust styles of home screen

--- components/HomeScreen/index.tsx
+++ components/HomeScreen/index.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import { HomeContainer } from './styles'
 import banner from '../../assets/banner.jpg'
+import { ProductStore } from './components/ProductStore'
 
-export function HomeScreen(){
-  return(
+export function HomeScreen() {
+  return (
     <HomeContainer>
-      <Image src={banner} alt="" title="Credits: Gradienta - Unsplash" />
+      <Image src={banner} alt="" title="Credits: Gradienta - Unsplash" width={500} height={500} />
+      <ProductStore />
     </HomeContainer>
   )
 }
--- components/HomeScreen/styles.ts
+++ components/HomeScreen/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
 export const HomeContainer = styled.div`
+    max-width: 100%;
+
     display: flex;
     flex-direction: column;
     align-items: center;
