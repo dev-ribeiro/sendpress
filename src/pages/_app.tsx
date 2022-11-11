2734841830100e3e3b72d538de05668d0cc50a1a
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-11 20:09:39 -0300

    styles: move styles folder to src

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from '../../styles/global'
-import { defaultTheme } from '../../styles/themes/defaultTheme'
+import { GlobalStyle } from '../styles/global'
+import { defaultTheme } from '../styles/themes/defaultTheme'
 import { ProductContextProvider } from '../contexts/ProductContexts'
 
 function MyApp({ Component, pageProps }: AppProps) {
