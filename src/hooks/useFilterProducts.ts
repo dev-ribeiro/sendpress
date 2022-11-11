d90ce2afbb0194e8a978d74064122fb061023161
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-10 22:36:50 -0300

    hooks: create useFilterProducts hook

---
+++ src/hooks/useFilterProducts.ts
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+type FilterValuesType = 'todos' | 'chaveiros' | 'acessórios' | 'escritório' | 'outros'
+
+export function useFilterProducts() {
+  const [filter, setFilter] = useState<FilterValuesType>('todos')
+
+  function handleFilterProduct(desiredFilter: FilterValuesType) {
+    switch (desiredFilter) {
+    case 'acessórios':
+      setFilter('acessórios')
+      break
+    case 'chaveiros':
+      setFilter('chaveiros')
+      break
+    case 'escritório':
+      setFilter('escritório')
+      break
+    case 'outros':
+      setFilter('outros')
+      break
+    case 'todos':
+      setFilter('todos')
+      break
+    default:
+      return filter
+    }
+  }
+
+  return {
+    filter,
+    handleFilterProduct
+  }
+}
